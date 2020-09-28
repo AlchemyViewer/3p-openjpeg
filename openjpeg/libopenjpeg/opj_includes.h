2ec7d06afb325ef524918d6efafff40678cebe65
Rye Mutt <rye@alchemyviewer.org>
2020-09-27 20:03:07 -0400

    Backport malloc code from openjpeg2

--- openjpeg/libopenjpeg/opj_includes.h
+++ openjpeg/libopenjpeg/opj_includes.h
@@ -80,24 +80,6 @@
 	#define __attribute__(x) /* __attribute__(x) */
 #endif
 
-/*
-The inline keyword is supported by C99 but not by C90.
-Most compilers implement their own version of this keyword ...
-*/
-#ifndef INLINE
-	#if defined(_MSC_VER)
-		#define INLINE __forceinline
-	#elif defined(__GNUC__)
-		#define INLINE __inline__
-	#elif defined(__MWERKS__)
-		#define INLINE inline
-	#else
-		/* add other compilers here ... */
-		#define INLINE
-	#endif /* defined(<Compiler>) */
-#endif /* INLINE */
-
-
 
 /* MSVC before 2013 and Borland C do not have lrintf */
 #if defined(_MSC_VER)
@@ -179,6 +161,11 @@ static INLINE long opj_lrintf(float f)
 #       endif
 #   endif
 #endif
+
+/* Type to use for bit-fields in internal headers */
+typedef unsigned int OPJ_BITFIELD;
+
+#define OPJ_UNUSED(x) (void)x
 
 #include "j2k_lib.h"
 #include "opj_malloc.h"
